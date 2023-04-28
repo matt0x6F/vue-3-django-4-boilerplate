5df0df33e2091a90d39cc7a44ed671e3c23a7075
gpabloandres <gpabloandres@gmail.com>
2023-04-27 21:40:59 -0300

    Inicio Repo

---
+++ vuefront/src/main.js
@@ -0,0 +1,15 @@
+import { createApp } from 'vue'
+
+import axios from 'axios'
+
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.config.globalProperties.$axios = axios
+window.axios = axios
+
+app.use(router)
+
+app.mount('#app')
---
+++ vuefront/src/pages/ListCategory.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Listado</h1>
+</template>
+<script>
+export default {
+    mounted(){
+        this.$axios.get("http://localhost:8000/api/products/?format=json").then((res)=>{
+            console.log(res)
+        })
+    }
+}
+</script>
